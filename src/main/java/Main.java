bcd6475f05278f87c8debfb47f73d1af1e8659c1
Giaquinto Raphael <giaquinto@b11p17.fil.univ-lille1.fr>
2019-09-20 15:19:52 +0200

    implémentation d'une superbe fenêtre

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,15 @@
+import javax.swing.JFrame;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("2d game engine");
+		
+		JFrame frame = new JFrame("2d game engine");
+		frame.setSize(600,600);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setVisible(true);
 	}
 
 }
